685427613c7241d417ca10533f1d33ca070765f7
Harry Reeder <harry@harryreeder.co.uk>
2020-03-22 17:17:05 +0000

    👷‍♀️ Fix typo in ref-parser

--- util/ref-parser.js
+++ util/ref-parser.js
@@ -1,10 +1,17 @@
 const parts = process.env.ref.split("/");
 const branch_name = parts[parts.length-1];
 
+console.log(`Got This: ${process.env.ref}`);
+console.log(`Parsed this: ${branch_name}`);
+
+function setEnvVar(name, value) {
+    console.log(`::set-env name=${name}::${value}`);
+}
+
 if (branch_name === "master") {
-    console.log("::set-env name=K8S_NAMESPACE::kondo");
-    console.log("::set-env name=K8S_DIR::prod");
+    setEnvVar("K8S_NAMESPACE", "kondo");
+    setEnvVar("K8S_DIR", "prod");
 } else if (branch_name === "dev") {
-    console.log(":::set-env name=K8S_NAMESPACE::kondo-dev");
-    console.log(":::set-env name=K8S_DIR::dev");
+    setEnvVar("K8S_NAMESPACE", "kondo-dev");
+    setEnvVar("K8S_DIR", "dev");
 }
