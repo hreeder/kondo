2c70484c64f0badc3d26c2792ec6c3ab584e91ec
Harry Reeder <harry@harryreeder.co.uk>
2020-03-22 20:17:25 +0000

    🚧 Print Headers (to debug XFF)

--- auth_service/main.py
+++ auth_service/main.py
@@ -4,9 +4,16 @@ Pylint Disables:
     invalid-name - allow "app" to exist for flask usage
 """
 # pylint: disable=invalid-name
-from flask import Flask
+from pprint import pprint
+
+from flask import Flask, request
 
 from .discord_auth import AUTH
 
 app = Flask(__name__)
 app.register_blueprint(AUTH, url_prefix="/auth/discord")
+
+
+@app.before_request
+def print_headers():
+    pprint(dict(request.headers))
