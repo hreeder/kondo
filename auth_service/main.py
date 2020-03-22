55e511c1e25fa06103e1c82fccbe477b2454b849
Harry Reeder <harry@harryreeder.co.uk>
2020-03-22 22:04:47 +0000

    ✨ Add Prometheus Metrics

--- auth_service/main.py
+++ auth_service/main.py
@@ -8,6 +8,7 @@ from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from shared.kondo.health import HEALTH
+from shared.kondo.metrics import wrap_with_prometheus
 
 from .discord_auth import AUTH
 
@@ -20,3 +21,6 @@ app.register_blueprint(HEALTH, url_prefix="/auth/_health")
 # * X-Forwarded-Host
 # * X-Forwarded-For
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1, x_for=1)
+
+# Add prometheus /metrics endpoint
+app = wrap_with_prometheus(app)
---
+++ shared/kondo/metrics.py
@@ -0,0 +1,21 @@
+from pprint import pprint
+
+import prometheus_client
+
+from flask import request
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+REQUESTS = prometheus_client.Counter(
+    "kondo_requests_total", "All HTTP requests", ["method", "path"]
+)
+
+
+def prom_before_request():
+    method = request.environ["REQUEST_METHOD"]
+    path = request.environ["PATH_INFO"]
+    REQUESTS.labels(method, path).inc()
+
+
+def wrap_with_prometheus(app):
+    app.before_request_funcs.setdefault(None, []).append(prom_before_request)
+    return DispatcherMiddleware(app, {"/metrics": prometheus_client.make_wsgi_app()})
